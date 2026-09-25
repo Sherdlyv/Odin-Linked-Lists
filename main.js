07ce161bab8449d58352e9a09a7b4646af9ab8f0
Sherdlyv <sherdlyvr@gmail.com>
2026-09-25 14:11:15 -0700

    git commit -m "chore: remove node_modules from git tracking and apply gitignore"

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
+class Node {
+    constructor(value = null) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
 class linkList {
     constructor () {
+        this.head = null;
 
+    }
+
+     append(value) {
+        const newNode = new Node(value);
+
+        if (this.head === null) {
+            this.head = newNode;
+            return;
+
+        }
+
+        let actualNode = this.head ;
+        while (actualNode.next !== null) {
+             actualNode = actualNode.next;
+        }
+        
+    }
+
+    prepend (value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
     }
 }
 
 
-class node {
-    constructor(value = null, nextNode = null) {
-        this.value = null;
-        this.nextNode = null;
-    }
-}
+
