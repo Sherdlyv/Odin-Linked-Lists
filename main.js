93c0b3096be9c6c27e208464f6d2d7ba15d94697
Sherdlyv <sherdlyvr@gmail.com>
2026-09-24 19:46:05 -0700

    "feat: setup Node and LinkedList skeleton"

---
+++ main.js
@@ -0,0 +1,14 @@
+
+class linkList {
+    constructor () {
+
+    }
+}
+
+
+class node {
+    constructor(value = null, nextNode = null) {
+        this.value = null;
+        this.nextNode = null;
+    }
+}
